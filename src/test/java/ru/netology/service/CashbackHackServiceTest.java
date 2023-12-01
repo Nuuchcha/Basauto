9e26c135c1abc16137bbaea12179cf66276ff920
Korolev E <nuuchcha@bk.ru>
2023-12-01 11:24:38 +0500

    Initial commit

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,42 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashbackHackServiceTest {
+
+    @Test
+    public void checkingLimitValues() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+
+        int expected = 1;
+        int actual = service.remain(amount);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void checkingBoudaryValues() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int expected = 0;
+        int actual = service.remain(amount);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void checkingForeignValues() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1001;
+
+        int expected = 999;
+        int actual = service.remain(amount);
+
+        assertEquals(expected, actual);
+    }
+
+}
